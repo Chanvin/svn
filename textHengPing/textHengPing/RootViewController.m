7926a5a8b9d2e08441765248a4f4af4288cffd33
bosma <wengshaozhong@gmail.com>
2014-03-17 14:59:01 +0800

    Revert "change"
    This reverts commit a64be14405718aa21018d8e4f560d3a0e226017a.

--- textHengPing/textHengPing/RootViewController.m
+++ textHengPing/textHengPing/RootViewController.m
@@ -26,6 +26,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.navigationController.navigationBarHidden=YES;
+    self.view.backgroundColor = [UIColor whiteColor];
     [self _initButton];
 
 }
